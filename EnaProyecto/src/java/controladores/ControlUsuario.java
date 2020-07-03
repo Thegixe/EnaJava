bf2ee985f52aafcbbbcb8a1632a34faca2fdc5c9
TheGixe <TheGixe@TheGixe-PC>
2020-07-03 14:14:22 -0400

    creacion de config

---
+++ EnaProyecto/src/java/controladores/ControlUsuario.java
@@ -0,0 +1,115 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controladores;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.SQLException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modelos.Usuario;
+
+/**
+ *
+ * @author TheGixe
+ */
+@WebServlet(name = "ControlUsuario", urlPatterns = {"/ControlUsuario"})
+public class ControlUsuario extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        try{
+        String accion = request.getParameter("accion");        
+        switch(accion){            
+            case "1": iniciarSesion(request,response);
+            break;
+        }               
+        }
+        catch(Exception e){
+            response.sendRedirect("registro.jsp?mensaje="+e.getMessage());
+        }
+    }
+    
+    private void iniciarSesion(HttpServletRequest request, HttpServletResponse response) throws IOException, ClassNotFoundException, SQLException{
+        String usuario = request.getParameter("usuario").trim();
+        String password = request.getParameter("password").trim();
+        if(usuario.isEmpty()||password.isEmpty()){
+            response.sendRedirect("index.jsp?mensaje=Complete todos los campos");
+        }else{
+            Usuario u = new Usuario();
+            u.setUsuario(usuario);
+            u.setPassword(password);
+            if(u.iniciarSesion()){
+                HttpSession sesion = request.getSession();
+                sesion.setAttribute("usuario", u);
+                response.sendRedirect("intranet.jsp");
+            }else{
+                response.sendRedirect("index.jsp?mensaje=credenciales incorrectas");
+            }
+    }
+    }
+    
+    
+    
+    
+    
+    
+    
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- EnaProyecto/web/index.jsp
+++ EnaProyecto/web/index.jsp
@@ -9,9 +9,31 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-        <title>JSP Page</title>
+        <title>Inicio</title>
     </head>
-    <body>
-        <h1>Hello World!</h1>
+    <body><center>
+        <h1>Inicio de sesion</h1>
+        <form action="ControlUsuario" method="post">
+            
+                <table style="border: 1; ">
+                   
+                    <tr>
+                        <td>Usuario</td>
+                        <td><input type="text" name="usuario" /></td>
+                    </tr>
+                    <tr>
+                        <td>Password</td>
+                        <td><input type="password" name="password"/></td>
+                    </tr>
+                    <tr>
+                        <td><input type="submit" value="Ingresar"/></td>
+                        <input type="hidden" name="accion" value="4"/>
+                    </tr>
+                </table>
+           <% if(request.getParameter("mensaje")!=null){%>
+        <%=request.getParameter("mensaje") %>
+        <%}%>
+        </form>
+         </center>
     </body>
 </html>
